aa0b000c1d98fdadec28074e907d7b3f53ec50b0
M SMRUTI RANJAN <ranjanmsmruti@gmail.com>
2025-10-19 08:13:44 +0530

    modification

--- src/main/java/com/example/javaexecutorframework/AsyncScheduling/LongRunningTask.java
+++ src/main/java/com/example/javaexecutorframework/AsyncScheduling/LongRunningTask.java
@@ -5,21 +5,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LongRunningTask implements Runnable{
 
-    private String cmd;
+    private String param;
 
-    public LongRunningTask(String cmd)
+    public LongRunningTask(String param)
     {
-        this.cmd = cmd;
+        this.param = param;
     }
 
     @Override
     public void run() {
-        log.info("{} Stating Task..{}", cmd, Thread.currentThread().getName());
+        log.info("{} Stating Task..{}", param, Thread.currentThread().getName());
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        log.info("{} Ending Task..{}", cmd, Thread.currentThread().getName());
+        log.info("{} Ending Task..{}", param, Thread.currentThread().getName());
     }
 }
