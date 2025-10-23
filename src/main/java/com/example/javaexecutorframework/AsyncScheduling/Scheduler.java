7e9077746c6ab01bf6516a2ee1f2197c47de04e8
M SMRUTI RANJAN <ranjanmsmruti@gmail.com>
2025-10-23 05:36:36 +0530

    Async Task Scheduling using @Async.

--- src/main/java/com/example/javaexecutorframework/AsyncScheduling/Scheduler.java
+++ src/main/java/com/example/javaexecutorframework/AsyncScheduling/Scheduler.java
@@ -1,6 +1,7 @@
 package com.example.javaexecutorframework.AsyncScheduling;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +11,7 @@ public class Scheduler {
 
     //@Scheduled(fixedRate = 1000) //this method will now run exactly at an interval of 1sec. Not concurrent/ blocking in nature.
     @Scheduled(cron = "*/5 * * * * *") //scheduled to run in every 5 seconds.
+    @Async("jobExecutor")
     void logging() throws InterruptedException {
         log.info("Scheduler started..{}", Thread.currentThread().getName());
         Thread.sleep(2000);
@@ -17,6 +19,7 @@ public class Scheduler {
     }
 
     @Scheduled(fixedDelay = 2000) //this method will now run with a delay of 2 second means new task will start after 3 sec.
+    @Async("jobExecutor")
     void logging2() throws InterruptedException {
         log.info("Scheduler started with delay..{}", Thread.currentThread().getName());
         Thread.sleep(1000);
--- src/main/java/com/example/javaexecutorframework/AsyncScheduling/ThreadConfig.java
+++ src/main/java/com/example/javaexecutorframework/AsyncScheduling/ThreadConfig.java
@@ -1,12 +1,20 @@
 package com.example.javaexecutorframework.AsyncScheduling;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ExecutorConfigurationSupport;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Controller;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
 @Configuration
+@Slf4j
 public class ThreadConfig {
     @Bean
     public TaskScheduler taskScheduler()
@@ -16,5 +24,31 @@ public class ThreadConfig {
         taskScheduler.setThreadNamePrefix("TaskScheduler-");
         taskScheduler.initialize();
         return taskScheduler;
+    }
+
+    @Bean(name = "jobExecutor")
+    public Executor jobExecutor()
+    {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(4);
+        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setQueueCapacity(6);
+        taskExecutor.setThreadNamePrefix("AsyncTaskExec-");
+        taskExecutor.setRejectedExecutionHandler(new RejectedExecutionHandler() {
+            @Override
+            public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+                log.info("Thread rejected... Retrying...");
+                        try {
+                            Thread.sleep(2000);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        executor.submit(r);
+            }
+        });
+        taskExecutor.initialize();
+        return taskExecutor;
+
+
     }
 }
